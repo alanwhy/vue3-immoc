2506b368d199fe89f47a68779bf58eeb587f3c50
alanwhy <570028237@qq.com>
2021-02-15 21:42:04 +0800

    08-10.使用v-for, v-html 指令精简页面代码

--- jingdong/src/views/home/StaticPart.vue
+++ jingdong/src/views/home/StaticPart.vue
@@ -9,81 +9,15 @@
     <span class="search__text">山姆会员商店优惠商品</span>
   </div>
   <div class="banner">
-    <img
-      class="banner__img"
-      src="/img/banner.jpg"
-    />
+    <img class="banner__img" src="/img/banner.jpg" />
   </div>
   <div class="icons">
-    <div class="icons__item">
+    <div class="icons__item" v-for="item in iconsList" :key="item.desc">
       <img
         class="icons__item__img"
-        src="/img/超市.png"
+        :src="`/img/${item.imgName}.png`"
       />
-      <p class="icons__item__desc">超市便利</p>
-    </div>
-    <div class="icons__item">
-      <img
-        class="icons__item__img"
-        src="/img/菜市场.png"
-      />
-      <p class="icons__item__desc">菜市场</p>
-    </div>
-    <div class="icons__item">
-      <img
-        class="icons__item__img"
-        src="/img/水果店.png"
-      />
-      <p class="icons__item__desc">水果店</p>
-    </div>
-    <div class="icons__item">
-      <img
-        class="icons__item__img"
-        src="/img/鲜花.png"
-      />
-      <p class="icons__item__desc">鲜花绿植</p>
-    </div>
-    <div class="icons__item">
-      <img
-        class="icons__item__img"
-        src="/img/医药健康.png"
-      />
-      <p class="icons__item__desc">医药健康</p>
-    </div>
-    <div class="icons__item">
-      <img
-        class="icons__item__img"
-        src="/img/家居.png"
-      />
-      <p class="icons__item__desc">家居时尚</p>
-    </div>
-    <div class="icons__item">
-      <img
-        class="icons__item__img"
-        src="/img/蛋糕.png"
-      />
-      <p class="icons__item__desc">烘培蛋糕</p>
-    </div>
-    <div class="icons__item">
-      <img
-        class="icons__item__img"
-        src="/img/签到.png"
-      />
-      <p class="icons__item__desc">签到</p>
-    </div>
-    <div class="icons__item">
-      <img
-        class="icons__item__img"
-        src="/img/大牌免运.png"
-      />
-      <p class="icons__item__desc">大牌免运</p>
-    </div>
-    <div class="icons__item">
-      <img
-        class="icons__item__img"
-        src="/img/红包.png"
-      />
-      <p class="icons__item__desc">红包套餐</p>
+      <p class="icons__item__desc">{{ item.desc }}</p>
     </div>
   </div>
   <div class="gap"></div>
@@ -91,46 +25,61 @@
 
 <script>
 export default {
-  name: 'StaticPart'
-}
+  name: "StaticPart",
+  setup() {
+    const iconsList = [
+      { imgName: "超市", desc: "超市便利" },
+      { imgName: "菜市场", desc: "菜市场" },
+      { imgName: "水果店", desc: "水果店" },
+      { imgName: "鲜花", desc: "鲜花绿植" },
+      { imgName: "医药健康", desc: "医药健康" },
+      { imgName: "家居", desc: "家居时尚" },
+      { imgName: "蛋糕", desc: "烘培蛋糕" },
+      { imgName: "签到", desc: "签到" },
+      { imgName: "大牌免运", desc: "大牌免运" },
+      { imgName: "红包", desc: "红包套餐" },
+    ];
+    return { iconsList };
+  },
+};
 </script>
 
 <style lang="scss">
-@import '../../style/viriables.scss';
-@import '../../style/mixins.scss';
+@import "../../style/viriables.scss";
+@import "../../style/mixins.scss";
 .position {
   position: relative;
-  padding: .16rem .24rem .16rem 0;
-  line-height: .22rem;
-  font-size: .16rem;
+  padding: 0.16rem 0.24rem 0.16rem 0;
+  line-height: 0.22rem;
+  font-size: 0.16rem;
   @include ellipsis;
   .position__icon {
     position: relative;
-    top: .01rem;
-    font-size: .2rem;
+    top: 0.01rem;
+    font-size: 0.2rem;
   }
   .position_notice {
     position: absolute;
     right: 0;
-    top: .17rem;
-    font-size: .2rem;
+    top: 0.17rem;
+    font-size: 0.2rem;
   }
   color: $content-fontcolor;
 }
 .search {
-  margin-bottom: .12rem;
-  line-height: .32rem;
-  background: #F5F5F5;
-  color: #B7B7B7;
-  border-radius: .16rem;
+  margin-bottom: 0.12rem;
+  line-height: 0.32rem;
+  background: #f5f5f5;
+  color: #b7b7b7;
+  border-radius: 0.16rem;
   .iconfont {
     display: inline-block;
-    padding: 0 .08rem 0 .16rem;
-    font-size: .16rem;
+    padding: 0 0.08rem 0 0.16rem;
+    font-size: 0.16rem;
   }
   &__text {
     display: inline-block;
-    font-size: .14rem;
+    font-size: 0.14rem;
   }
 }
 .banner {
@@ -144,25 +93,25 @@ export default {
 .icons {
   display: flex;
   flex-wrap: wrap;
-  margin-top: .16rem;
+  margin-top: 0.16rem;
   &__item {
     width: 20%;
     &__img {
-      display:block;
-      width: .4rem;
-      height: .4rem;
+      display: block;
+      width: 0.4rem;
+      height: 0.4rem;
       margin: 0 auto;
     }
     &__desc {
-      margin: .06rem 0 .16rem 0;
+      margin: 0.06rem 0 0.16rem 0;
       text-align: center;
       color: $content-fontcolor;
     }
   }
 }
 .gap {
-  margin: 0 -.18rem;
-  height: .1rem;
+  margin: 0 -0.18rem;
+  height: 0.1rem;
   background: $content-bgColor;
 }
 </style>
