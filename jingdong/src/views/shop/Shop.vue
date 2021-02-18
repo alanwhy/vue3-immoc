669d7f658c4f721dea61e44a6266677d99ce5975
alanwhy <570028237@qq.com>
2021-02-18 19:30:46 +0800

    10-06～10-07.路由参数的传递以及商家详情的获取

--- jingdong/src/views/shop/Shop.vue
+++ jingdong/src/views/shop/Shop.vue
@@ -7,43 +7,59 @@
         <input class="search__content__input" placeholder="请输入商品名称" />
       </div>
     </div>
-    <ShopInfo :item="item" :hideBorder="true" />
+    <ShopInfo :item="item" :hideBorder="true" v-show="item.imgUrl" />
   </div>
 </template>
 
 <script>
-import { useRouter } from "vue-router";
+import { reactive, toRefs } from "vue";
+import { useRouter, useRoute } from "vue-router";
+import { get } from "../../utils/request";
 import ShopInfo from "../../components/ShopInfo";
+
+// 获取当前商铺信息
+const useShopInfoEffect = () => {
+  const route = useRoute();
+  const data = reactive({ item: {} });
+  const getItemData = async () => {
+    const result = await get(`/api/shop/${route.params.id}`);
+    if (result?.errno === 0 && result?.data) {
+      data.item = result.data;
+    }
+  };
+  const { item } = toRefs(data);
+  return { item, getItemData };
+};
+
+// 点击回退逻辑
+const useBackRouterEffect = () => {
+  const router = useRouter();
+  const handleBackClick = () => {
+    router.back();
+  };
+  return handleBackClick;
+};
 
 export default {
   name: "Shop",
   components: { ShopInfo },
   setup() {
-    const router = useRouter();
-    const item = {
-      _id: "1",
-      name: "沃尔玛",
-      imgUrl: "http://www.dell-lee.com/imgs/vue3/near.png",
-      sales: 10000,
-      expressLimit: 0,
-      expressPrice: 5,
-      slogan: "VIP尊享满89元减4元运费券",
-    };
-    const handleBackClick = () => {
-      router.back();
-    };
+    const { item, getItemData } = useShopInfoEffect();
+    const handleBackClick = useBackRouterEffect();
+    getItemData();
     return { item, handleBackClick };
   },
 };
 </script>
 
 <style lang="scss" scoped>
+@import "../../style/viriables.scss";
 .wrapper {
   padding: 0 0.18rem;
 }
 .search {
   display: flex;
-  margin: 0.2rem 0 0.16rem 0;
+  margin: 0.14rem 0 0.04rem 0;
   line-height: 0.32rem;
   &__back {
     width: 0.3rem;
@@ -53,12 +69,12 @@ export default {
   &__content {
     display: flex;
     flex: 1;
-    background: #f5f5f5;
+    background: $search-bgColor;
     border-radius: 0.16rem;
     &__icon {
       width: 0.44rem;
       text-align: center;
-      color: #b7b7b7;
+      color: $search-fontColor;
     }
     &__input {
       display: block;
@@ -69,9 +85,9 @@ export default {
       background: none;
       height: 0.32rem;
       font-size: 0.14rem;
-      color: #333;
+      color: $content-fontcolor;
       &::placeholder {
-        color: #333;
+        color: $content-fontcolor;
       }
     }
   }
