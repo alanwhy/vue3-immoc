1c4b8e6b41f4091346572eb0f3c335e4e903728e
alanwhy <570028237@qq.com>
2021-02-20 17:49:42 +0800

    12-03.页面布局及展示逻辑开发

--- jingdong/src/views/orderConfirmation/OrderConfirmation.vue
+++ jingdong/src/views/orderConfirmation/OrderConfirmation.vue
@@ -2,7 +2,9 @@
   <div class="wrapper">
     <div class="top">
       <div class="top__header">
-        <div class="iconfont top__header__back">&#xe6f2;</div>
+        <div class="iconfont top__header__back" @click="handleBackClick">
+          &#xe6f2;
+        </div>
         确认订单
       </div>
       <div class="top__receiver">
@@ -21,38 +23,52 @@
       <div class="products__title">
         {{ shopName }}
       </div>
-      <div class="products__list">
-        <div class="products__item" v-for="item in productList" :key="item._id">
-          <img class="products__item__img" :src="item.imgUrl" />
-          <div class="products__item__detail">
-            <h4 class="products__item__title">{{ item.name }}</h4>
-            <p class="products__item__price">
-              <span>
-                <span class="products__item__yen">&yen; </span>
-                {{ item.price }} x {{ item.count }}
-              </span>
-              <span class="products__item__total">
-                <span class="products__item__yen">&yen; </span>
-                {{ item.price * item.count }}
-              </span>
-            </p>
-          </div>
+      <div class="products__wrapper">
+        <div class="products__list">
+          <template v-for="item in productList" :key="item._id">
+            <div v-if="item.count > 0" class="products__item">
+              <img class="products__item__img" :src="item.imgUrl" />
+              <div class="products__item__detail">
+                <h4 class="products__item__title">{{ item.name }}</h4>
+                <p class="products__item__price">
+                  <span>
+                    <span class="products__item__yen">&yen; </span>
+                    {{ item.price }} x {{ item.count }}
+                  </span>
+                  <span class="products__item__total">
+                    <span class="products__item__yen">&yen; </span>
+                    {{ item.price * item.count }}
+                  </span>
+                </p>
+              </div>
+            </div>
+          </template>
         </div>
       </div>
+    </div>
+    <div class="order">
+      <div class="order__price">
+        实付金额 <b>¥{{ calculations.price }}</b>
+      </div>
+      <div class="order__btn">提交订单</div>
     </div>
   </div>
 </template>
 
 <script>
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import { useCommonCartEffect } from "../../effects/cartEffects";
 export default {
   name: "OrderConfirmation",
   setup() {
+    const router = useRouter();
     const route = useRoute();
     const shopId = route.params.id;
-    const { shopName, productList } = useCommonCartEffect(shopId);
-    return { shopName, productList };
+    const { shopName, calculations, productList } = useCommonCartEffect(shopId);
+    const handleBackClick = () => {
+      router.back();
+    };
+    return { shopName, calculations, productList, handleBackClick };
   },
 };
 </script>
@@ -67,6 +83,7 @@ export default {
   top: 0;
   bottom: 0;
   background-color: #eee;
+  overflow-y: scroll;
 }
 .top {
   position: relative;
@@ -127,17 +144,29 @@ export default {
   }
 }
 .products {
-  margin: 0.16rem 0.18rem 0.55rem 0.18rem;
+  margin: 0.16rem 0.18rem 0.1rem 0.18rem;
   background: #fff;
   &__title {
-    padding: 0.16rem 0.16rem 0 0.16rem;
+    padding: 0.16rem;
     font-size: 0.16rem;
     color: #333;
+  }
+  &__wrapper {
+    overflow-y: scroll;
+    margin: 0 0.18rem;
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0.6rem;
+    top: 2.6rem;
+  }
+  &__list {
+    background: #fff;
   }
   &__item {
     position: relative;
     display: flex;
-    padding: 0.16rem;
+    padding: 0 0.16rem 0.16rem 0.16rem;
     &__img {
       width: 0.46rem;
       height: 0.46rem;
@@ -168,6 +197,30 @@ export default {
     &__yen {
       font-size: 0.12rem;
     }
+  }
+}
+
+.order {
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  height: 0.49rem;
+  line-height: 0.49rem;
+  background: #fff;
+  &__price {
+    flex: 1;
+    text-indent: 0.24rem;
+    font-size: 0.14rem;
+    color: #333;
+  }
+  &__btn {
+    width: 0.98rem;
+    background: #4fb0f9;
+    color: #fff;
+    text-align: center;
+    font-size: 0.14rem;
   }
 }
 </style>
