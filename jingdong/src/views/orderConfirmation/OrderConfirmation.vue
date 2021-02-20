184e2618f6f965bc17cc013c99e7ca7b1b662e6f
alanwhy <570028237@qq.com>
2021-02-20 17:34:10 +0800

    12-02.订单商品列表数据获取与布局

--- jingdong/src/views/orderConfirmation/OrderConfirmation.vue
+++ jingdong/src/views/orderConfirmation/OrderConfirmation.vue
@@ -17,16 +17,49 @@
         <div class="iconfont top__receiver__icon">&#xe6f2;</div>
       </div>
     </div>
+    <div class="products">
+      <div class="products__title">
+        {{ shopName }}
+      </div>
+      <div class="products__list">
+        <div class="products__item" v-for="item in productList" :key="item._id">
+          <img class="products__item__img" :src="item.imgUrl" />
+          <div class="products__item__detail">
+            <h4 class="products__item__title">{{ item.name }}</h4>
+            <p class="products__item__price">
+              <span>
+                <span class="products__item__yen">&yen; </span>
+                {{ item.price }} x {{ item.count }}
+              </span>
+              <span class="products__item__total">
+                <span class="products__item__yen">&yen; </span>
+                {{ item.price * item.count }}
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { useRoute } from "vue-router";
+import { useCommonCartEffect } from "../../effects/cartEffects";
 export default {
   name: "OrderConfirmation",
+  setup() {
+    const route = useRoute();
+    const shopId = route.params.id;
+    const { shopName, productList } = useCommonCartEffect(shopId);
+    return { shopName, productList };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+@import "../../style/viriables.scss";
+@import "../../style/mixins.scss";
 .wrapper {
   position: absolute;
   left: 0;
@@ -90,6 +123,50 @@ export default {
       top: 0.5rem;
       color: #666;
       font-size: 0.2rem;
+    }
+  }
+}
+.products {
+  margin: 0.16rem 0.18rem 0.55rem 0.18rem;
+  background: #fff;
+  &__title {
+    padding: 0.16rem 0.16rem 0 0.16rem;
+    font-size: 0.16rem;
+    color: #333;
+  }
+  &__item {
+    position: relative;
+    display: flex;
+    padding: 0.16rem;
+    &__img {
+      width: 0.46rem;
+      height: 0.46rem;
+      margin-right: 0.16rem;
+    }
+    &__detail {
+      flex: 1;
+    }
+    &__title {
+      margin: 0;
+      line-height: 0.2rem;
+      font-size: 0.14rem;
+      color: $content-fontcolor;
+      @include ellipsis;
+    }
+    &__price {
+      display: flex;
+      margin: 0.06rem 0 0 0;
+      line-height: 0.2rem;
+      font-size: 0.14rem;
+      color: $hightlight-fontColor;
+    }
+    &__total {
+      flex: 1;
+      text-align: right;
+      color: #000;
+    }
+    &__yen {
+      font-size: 0.12rem;
     }
   }
 }
